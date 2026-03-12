4d342b18d9db4eb61434dc1af2ae888ee99cc370
tnrao-trainer <40280456+tnrao-trainer@users.noreply.github.com>
2026-03-12 11:07:09 +0530

    Add files via upload

---
+++ 04.Week-4/19.Day19_C#_Methods_OOPs/Session Examlpes/Eg1_BasicDemo_Methods_Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace ConsoleApp35
+{ 
+    internal class Program
+    {
+
+        static void Greeting()
+        {
+            Console.WriteLine("Welcome to C# Methods");
+        }
+
+        static void Greeting(string  uname)
+        {
+            Console.WriteLine($"Hi {uname}, Good morning...!");
+        }
+
+        static int GetSum(int x, int y)
+        {
+            int z = x + y;
+            return z;
+        }
+
+
+        static string GetCurrentTime()
+        {
+            string str = DateTime.Now.ToString("T");
+            return str;
+        }
+
+        static void Main(string[] args)
+        {
+
+            Greeting();
+            Greeting();
+            Console.WriteLine("---------------------------");
+
+            Greeting("Narasimha");
+            Greeting("Scott");
+            Console.WriteLine("---------------------------");
+
+            Console.WriteLine("Sum Result : " + GetSum(10,20));
+            Console.WriteLine("Sum Result : " + GetSum(402,503));
+
+            Console.WriteLine("---------------------------");
+
+            Console.WriteLine("Current Time : " + GetCurrentTime());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 04.Week-4/19.Day19_C#_Methods_OOPs/Session Examlpes/Eg2_Parameter_Types_Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace ConsoleApp35
+{ 
+    internal class Program
+    {
+
+        // Default / Optional Parameters
+        static void GetTotalAmount(string productName, double unitPrice, int qty = 1)
+        {
+            double totalAmount = unitPrice * qty;
+            Console.WriteLine($"{productName}, {qty}, {unitPrice}, {totalAmount}");
+        }
+
+        // Usage of Params :  variable no. of params
+        static void GetSum(params int[]  numbers)
+        {
+            int s = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                s += numbers[i];
+            }
+
+            Console.WriteLine("Sum Result " + s);
+        }
+
+        // Usage of ref keyword ---  Call by Reference
+        static void Swap(ref int x, ref int y)
+        {
+            int z = x;
+            x = y;
+            y = z;
+        }
+
+
+        // Usage of out 
+        static void Divide(int dividend, int divisor, out int quotient, out int reminder)
+        {
+            quotient = dividend / divisor;
+            reminder = dividend % divisor;
+        }
+
+        static void Main(string[] args)
+        {
+
+            // Optional Parameters
+            GetTotalAmount("Laptop", 30000, 2);
+            GetTotalAmount("Mobile", 25000);
+            GetTotalAmount("Tab", 45000);
+
+            Console.WriteLine("-------------------------------");
+
+            GetSum(10);
+            GetSum(10, 20, 30);
+            GetSum(10, 20, 30, 40, 50, 60);
+            GetSum(10, 20);
+            GetSum(10, 20, 30, 40);
+            Console.WriteLine("-------------------------------");
+
+            // Usage of ref
+            int a = 10, b = 20;
+            Console.WriteLine($"Before Swapping :   A = {a}, B = {b}");
+            Swap(ref a, ref b);
+            Console.WriteLine($"After Swapping  :   A = {a}, B = {b}");
+
+            Console.WriteLine("---------------------------------------");
+
+            int x = 34, y = 5;
+          //  int resultQuotient, resultReminder;
+
+            Divide(x, y, out int resultQuotient, out int resultReminder);
+
+            Console.WriteLine($"Quotient : {resultQuotient}");
+            Console.WriteLine($"Reminder : {resultReminder}");
+
+            Console.ReadLine();
+        }
+    }
+}
