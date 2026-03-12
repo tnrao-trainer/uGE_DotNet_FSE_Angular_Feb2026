e7759718bacd354e4eda8abb1ae5e0b4db6e72be
tnrao-trainer <40280456+tnrao-trainer@users.noreply.github.com>
2026-03-12 12:58:10 +0530

    Add files via upload

---
+++ 04.Week-4/19.Day19_C#_Methods_OOPs/Session Examlpes/Eg3_OOPS_BasicClass_Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp35
+{
+
+    class Person
+    {
+        // data members 
+        private string _name = "Scott";
+        private int _age = 25;
+
+
+        // methods 
+        public void PrintDetails()
+        {
+            Console.WriteLine($"Name :  {_name}, Age : {_age}");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             Person personObj = new Person();
+             personObj.PrintDetails();
+            
+             Console.ReadLine();
+        }
+    } 
+}
---
+++ 04.Week-4/19.Day19_C#_Methods_OOPs/Session Examlpes/Eg4_OOPS_Constructors_Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace ConsoleApp35
+{
+
+    class Person
+    {
+        // data members 
+        private string _name;
+        private int _age;
+
+
+        public Person() { 
+            _name = string.Empty;   
+            _age = 0;           
+        }
+
+        public Person(string name)
+        {
+            _name = name;
+            _age = 0;
+        }
+
+        public Person(string name, int age)
+        {
+            _name = name;
+            _age = age;
+        }
+
+        // methods 
+        public void PrintDetails()
+        {
+            Console.WriteLine($"Name :  {_name}, Age : {_age}");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             Person p1 = new Person();
+             Person p2 = new Person("Scott");
+             Person p3 = new Person("Smith", 26);
+
+
+
+            p1.PrintDetails();
+            p2.PrintDetails();
+            p3.PrintDetails();
+
+             
+            
+            Console.ReadLine();
+        }
+    } 
+}
