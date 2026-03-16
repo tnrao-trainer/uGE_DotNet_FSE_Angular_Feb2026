cfff8b23f23f7c540c1de1e0018fc912e0d32770
tnrao-trainer <40280456+tnrao-trainer@users.noreply.github.com>
2026-03-16 11:35:30 +0530

    Add files via upload

---
+++ 05.Week-5/21.Day21_C#_OOPs/Session Examples/Eg2_Program_Usage_Of_Base_Keyword.cs
@@ -0,0 +1,50 @@
+﻿namespace ConsoleApp39
+{    
+    class Person
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+		
+     // Constructors using expression body
+        // public Person() => Name = "";
+        public Person(string name) => Name = name;
+ 
+        // Methods using expression body
+        public virtual void ShowDetails() =>  Console.WriteLine($"Name : {Name}, Age : {Age}");
+             
+    }
+    
+
+    class Employee : Person
+    {
+        public int EmployeeId { get; set; }
+        public string Job { get; set; }
+
+        public Employee(int id, string name) : base(name)
+        {
+            EmployeeId = id;
+        }
+
+        public override void ShowDetails()
+        {
+            base.ShowDetails();
+            Console.WriteLine($"Employee Id : {EmployeeId}, Job : {Job}");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             
+            Employee employee = new Employee(6445454, "Narasimha Rao");
+            employee.Job = "Manager";          
+            employee.Age = 32;
+
+
+            employee.ShowDetails();
+            
+            Console.ReadLine();
+        }
+    }
+}
